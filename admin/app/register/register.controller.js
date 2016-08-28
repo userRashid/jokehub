8da5d079ef10c4db539c5d2cba4af7d204fa2506
Rashid <user.rashid@gmail.com>
2016-08-28 11:06:54 +0530

    - add user register

--- admin/app/register/register.controller.js
+++ admin/app/register/register.controller.js
@@ -1,15 +1,19 @@
 (function() {
-	
+
 	'use strict';
 
 	angular
 		.module('app.register')
 		.controller('RegisterController', RegisterController);
 
-	function RegisterController() {
+	function RegisterController(API) {
 
         var vm = this;
-        
-    }
+        vm.Register = function(registerData){
+          API._post('register',registerData).then(function(){
+
+          });
+        }
+  }
 
 })();
