5ac5aef21dcbd15fe51147d105f5edf0d6cbed7e
Rashid <user.rashid@gmail.com>
2016-09-09 00:10:35 +0530

    - manage login/registor

--- admin/app/index.run.js
+++ admin/app/index.run.js
@@ -7,16 +7,32 @@
         .run(runBlock);
 
     /** @ngInject */
-    runBlock.$inject = ['$rootScope', '$state', 'Session', '$timeout'];
-    function runBlock($rootScope, $state, Session, $timeout) {
+    runBlock.$inject = ['$rootScope', '$state', 'Session'];
+
+    ///////////////////////////////////////
+    // Locals
+
+    function getUrl(url){
+      return url.split("#")[1];
+    }
+
+    function runBlock($rootScope, $state, Session) {
       $rootScope.$on('$locationChangeStart',function(event, next, current){
-        $timeout(function(){
-          var restrictedPage = $.inArray($state.current.url, ['/login', '/register']) === -1;
-          var loggedIn = Session.get('ticket') !== '' && Session.get('ticket') !== null;
-          if(!loggedIn) {
+        var previousUrl = getUrl(current)
+          ,currentUrl = getUrl(next)
+          ,_url = '';
+        if(previousUrl === currentUrl){
+          _url = '/home';
+        } else {
+          _url = previousUrl;
+        }
+        var restrictedPage = $.inArray(currentUrl, ['/login', '/register']) === -1;
+        var loggedIn = Session.get('ticket') !== '' && Session.get('ticket') !== null;
+        if(!loggedIn && restrictedPage){
             $state.go('app.login',{});
-          }
-        })
+        } else {
+          //console.log('Login IN',restrictedPage,currentUrl);
+        }
       });
     }
 })();
