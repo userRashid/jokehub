0604dd5c9551299fd6c86cffe2201a8ab0037e2a
Rashid <user.rashid@gmail.com>
2018-05-11 14:43:57 +0530

    - removed some files

--- include/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="py-2 border-top">
-    <div class="container text-center">
-        <p class="m-0 text-center">Copyright &copy; TheJokeHub.com 2018</p>
-        <a href="/privacy-policy" class="badge">Privacy</a>
-        <a href="#" class="badge">Disclaimer</a>
-        <a href="#" class="badge">About us</a>
-    </div>
-</footer>
