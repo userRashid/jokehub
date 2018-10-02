1169233074906b9a701725d205c63fc323b3c0b2
Rashid <user.rashid@gmail.com>
2018-10-03 00:20:36 +0530

    - distributed route into 3 sub route

--- admin/app/views/joke/Joke.Module.js
+++ admin/app/views/joke/Joke.Module.js
@@ -13,11 +13,48 @@
                 }
             }
         }).state('jokehub.manageJoke', {
+            abstract: true,
             url: '/manage-joke',
             views: {
                 'content@jokehub': {
                     templateUrl: 'app/views/joke/manage-joke/manage-joke.html',
                     controller: 'ManageJokeController as vm'
+                }
+            }
+        }).state('jokehub.manageJoke.approved', {
+            url: '/approved',
+            views: {
+                'content@jokehub': {
+                    templateUrl: 'app/views/joke/manage-joke/manage-joke.html',
+                    controller: 'ManageJokeController as vm'
+                },
+                'managejoke@jokehub.manageJoke.approved': {
+                    templateUrl: 'app/views/joke/manage-joke/approved/approved.html',
+                    controller: 'ApprovedJokeController as vm'
+                }
+            }
+        }).state('jokehub.manageJoke.pending', {
+            url: '/pending',
+            views: {
+                'content@jokehub': {
+                    templateUrl: 'app/views/joke/manage-joke/manage-joke.html',
+                    controller: 'ManageJokeController as vm'
+                },
+                'managejoke@jokehub.manageJoke.pending': {
+                    templateUrl: 'app/views/joke/manage-joke/pending/pending.html',
+                    controller: 'PendingJokeController as vm'
+                }
+            }
+        }).state('jokehub.manageJoke.rejected', {
+            url: '/rejected',
+            views: {
+                'content@jokehub': {
+                    templateUrl: 'app/views/joke/manage-joke/manage-joke.html',
+                    controller: 'ManageJokeController as vm'
+                },
+                'managejoke@jokehub.manageJoke.rejected': {
+                    templateUrl: 'app/views/joke/manage-joke/rejected/rejected.html',
+                    controller: 'RejectedJokeController as vm'
                 }
             }
         });
